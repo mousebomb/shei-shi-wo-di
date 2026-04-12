d83551bc6cb3820559316accc709fc7fa80b7a9c
mousebomb <mousebomb@gmail.com>
2026-04-12 21:25:09 +0800

    关闭啰嗦报错

--- backend/src/manager/VoiceManager.ts
+++ backend/src/manager/VoiceManager.ts
@@ -1,5 +1,5 @@
 // 定义请求的 URL
-import {CosyVoice_API} from "../constants";
+import {CosyVoice_API, LLM_LOG_V} from "../constants";
 import axios from "axios";
 import FormData from 'form-data';
 
@@ -35,12 +35,12 @@ export default class VoiceManager {
 
                 return uint8Array;
             } else {
-                console.error('音频合成失败: 服务器返回状态码', response.status);
+                LLM_LOG_V && console.error('音频合成失败: 服务器返回状态码', response.status);
                 // throw new Error(`音频合成失败: 服务器返回状态码 ${response.status}`);
                 return null;
             }
         } catch (error) {
-            console.error('音频合成请求失败:', error);
+            LLM_LOG_V && console.error('音频合成请求失败:', error);
             // throw error;
             return null;
         }
